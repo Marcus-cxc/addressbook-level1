22b0d45b7ce82d02ec659a1da607f8a01965648c
Marcus Chen <Marcus_cxc@outlook.com>
2018-02-01 23:42:02 +0800

    AddressBook.java: Improve readability for executeDeletePerson() method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -516,25 +516,21 @@ public class AddressBook {
      * @return feedback display message for the operation result
      */
     private static String executeDeletePerson(String commandArgs) {
-        if (isDeletePersonArgsValid(commandArgs)) {
-
-            final int targetVisibleIndex = extractTargetIndexFromDeletePersonArgs(commandArgs);
-
-            if (isDisplayIndexValidForLastPersonListingView(targetVisibleIndex)) {
-
-                final String[] targetInModel = getPersonByLastVisibleIndex(targetVisibleIndex);
-                return deletePersonFromAddressBook(targetInModel) ? getMessageForSuccessfulDelete(targetInModel) // success
-                        : MESSAGE_PERSON_NOT_IN_ADDRESSBOOK; // not found
-            }
-            else
-            {
-                return MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
-            }
-        }
-        else
-        {
+        boolean invalidInput = !isDeletePersonArgsValid(commandArgs);
+        if (invalidInput) { //Guard Clause
             return getMessageForInvalidCommandInput(COMMAND_DELETE_WORD, getUsageInfoForDeleteCommand());
         }
+
+        final int targetVisibleIndex = extractTargetIndexFromDeletePersonArgs(commandArgs);
+        boolean invalidIndex = !isDisplayIndexValidForLastPersonListingView(targetVisibleIndex);
+
+        if (invalidIndex) { //Guard Clause
+            return MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+        }
+
+        final String[] targetInModel = getPersonByLastVisibleIndex(targetVisibleIndex);
+        return deletePersonFromAddressBook(targetInModel) ? getMessageForSuccessfulDelete(targetInModel) // success
+                : MESSAGE_PERSON_NOT_IN_ADDRESSBOOK; // not found
 
     }
 
